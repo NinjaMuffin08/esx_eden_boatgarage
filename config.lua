f72b29ff1821294bacc134673154ad0f3eea5922
William Plunkett Jr <joey.plunkett92@yahoo.com>
2018-05-22 13:17:29 -0500

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,29 @@
+Config = {
+	DrawDistance = 100,
+	Price = 7500,
+	BlipInfos = {
+		Sprite = 290,
+		Color = 5 
+	}
+}
+
+Config.BoatGarages = {
+	Center = {	
+		Pos = {x=-735.87, y=-1325.08, z=1.5},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 204, g = 204, b = 0},
+		Marker = 1,
+		SpawnPoint = {
+			Pos = {x=-719.07598876953, y=-1320.7919921875, z=-0.49754849076271},
+			Color = {r=0,g=255,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		},
+		DeletePoint = {
+			Pos = {x=-731.15, y=-1334.71, z=-0.47477427124977},
+			Color = {r=255,g=0,b=0},
+			Size  = {x = 3.0, y = 3.0, z = 1.0},
+			Marker = 1
+		}, 	
+	},
+}
